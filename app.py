e326703418bf5fdd70610b27f267454d92be3322
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2023-07-25 10:14:16 +0600

    Third push Test

---
+++ app.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken()
+myChineseChef.make_fried_rice()
